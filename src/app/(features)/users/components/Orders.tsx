4ae478893cd5a481ce92c28e296ca3907d3b6fb6
ZZChaser <chaserjustdoit@gmail.com>
2023-12-01 17:44:31 +0800

    feat: 添加 order 组件

--- src/app/(features)/users/components/Orders.tsx
+++ src/app/(features)/users/components/Orders.tsx
@@ -1,12 +1,18 @@
+import { useOrderContext } from '@/stores/order';
 import Order from './Order';
 
 export function Orders() {
+  const { orders } = useOrderContext();
+
   return (
     <section>
       <h2>Orders</h2>
+
       <ul>
         {orders.map((order) => (
-          <Order key={order.created} {...order} />
+          <li key={order.created}>
+            {order.created} | {order.total / 100} ₽ | {order.status}
+          </li>
         ))}
       </ul>
     </section>
